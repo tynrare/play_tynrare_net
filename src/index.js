36858ccd1b7ea9d8cdce99f3336dc821bfecefe4
tynrare <tynrare@gmail.com>
2022-04-22 22:09:56 +0300

    220422 fin

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import { dust0 } from './dust-0.js';
 import { Messages } from './messages.js';
 import initHilo from './render/index.js';
 
+function parseArgList(str) {
+	const separator = str.includes('\n') ? '\n' : ' ';
+	return str.split(separator);
+}
+const dustdb = {};
+
 class Dust0 {
 	constructor() {
 		this.active = false;
@@ -19,14 +25,12 @@ class Dust0 {
 		this.dbpalette = document.querySelector('db#palette-' + palette);
 
 		if (!this.dbpalette) {
-			throw new Error(`db#palette-pineapple32 wasn't found`);
+			throw new Error(`db#palette-${palette} wasn't found`);
 		}
 
-		const arg = this.dbpalette.innerHTML;
-		const separator = arg.includes('\n') ? '\n' : ' ';
-		this.palette = arg.split(separator);
+		this.palette = parseArgList(this.dbpalette.innerHTML);
 
-		console.log(`Palette "${palette}" loaded`, this.palette);
+		//console.log(`Palette "${palette}" loaded`, this.palette);
 
 		return this;
 	}
@@ -35,14 +39,20 @@ class Dust0 {
 		this.active = true;
 
 		for (let i = 0; i < this.tiles.length; i++) {
-			this.registerTileInteractive(this.tiles[i]);
+			this.registerTileInteractive(this.tiles[i], i);
 		}
 
 		this.step();
 	}
 
-	registerTileInteractive(tile) {
-		tile.addEventListener('click', (event) => tile.classList.toggle('clicked'));
+	registerTileInteractive(tile, index) {
+		tile.addEventListener('click', (event) => {
+			this.cache.step = index;
+			tile.classList.toggle('clicked');
+
+			const palette = dustdb.palettes[Math.floor(Math.random() * dustdb.palettes.length)];
+			this.init(palette);
+		});
 	}
 
 	step() {
@@ -71,7 +81,7 @@ function main() {
 
 	const pixels = 512;
 	const tile = 64;
-	const tiles =  Math.pow(pixels / tile, 2);
+	const tiles = Math.pow(pixels / tile, 2);
 
 	let content = '';
 	for (let i = 0; i < tiles; i++) {
@@ -81,6 +91,7 @@ function main() {
 	messages.find('game').write(content);
 
 	dust0();
+	dustdb.palettes = parseArgList(document.querySelector('db#palette_list').innerHTML);
 	new Dust0().init('tyndustre_220422_00').exec();
 
 	//initHilo();
