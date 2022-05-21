9e20adeae39a1e43b1308aa42c74f60194892224
tynrare <tynrare@gmail.com>
2022-05-21 21:05:54 +0300

    2048 base created

--- src/index.js
+++ src/index.js
@@ -1,75 +1,154 @@
-import { dust0 } from './dust-0.js';
 import { Messages } from './messages.js';
 import initHilo from './render/index.js';
+import Alea from './lib/Alea.js';
 
-function parseArgList(str) {
-	const separator = str.includes('\n') ? '\n' : ' ';
-	return str.split(separator);
+const random = new Alea(0);
+
+function cssAnimate(element, name, duration) {
+	element.classList.add(name);
+	setTimeout(() => {
+		element.classList.remove(name);
+	}, duration);
 }
-const dustdb = {};
 
-class Dust0 {
+class Game2048Tile {
 	constructor() {
-		this.active = false;
-		this.cache = {
-			step: 0
-		};
+		this.pow = 1;
 	}
 
-	/**
-	 * @param {string} [palette='pineapple32'] .
-	 * @returns Dust0 this
-	 */
-	init(palette = 'pineapple32') {
-		this.tiles = document.querySelectorAll('tile');
-		this.dbpalette = document.querySelector('db#palette-' + palette);
+	get title() {
+		return Math.pow(2, this.pow);
+	}
+}
 
-		if (!this.dbpalette) {
-			throw new Error(`db#palette-${palette} wasn't found`);
-		}
+class Game2048 {
+	constructor(container, gridsize = 4) {
+		this.container = container;
+		this.gridsize = gridsize;
+	}
 
-		this.palette = parseArgList(this.dbpalette.innerHTML);
-
-		//console.log(`Palette "${palette}" loaded`, this.palette);
+	init() {
+		this.turn = 0;
+		this.tiles = Array.apply(null, Array(Math.pow(this.gridsize, 2))).map(() => null);
 
 		return this;
 	}
 
-	exec() {
-		this.active = true;
+	run() {
+		this.addNewTile();
+		this.draw();
 
-		for (let i = 0; i < this.tiles.length; i++) {
-			this.registerTileInteractive(this.tiles[i], i);
+		return this;
+	}
+
+	step(dirx = 0, diry = 0) {
+		this.turn += 1;
+
+		const hasSpace = this.tiles.includes(null);
+
+		let moved = true;
+		let movedAny = false;
+		while (moved) {
+			moved = false;
+			const direction = Math.sign(dirx || 1) * Math.sign(diry || 1) * -1;
+			for (
+				let i = direction > 0 ? 0 : this.tiles.length - 1;
+				i < this.tiles.length && i >= 0;
+				i += direction
+			) {
+				if (this.tiles[i]) {
+					moved = this.moveTile(i, dirx, diry) || moved;
+				}
+			}
+
+			if (!hasSpace && !moved) {
+				return;
+			}
+
+			movedAny = moved || movedAny;
 		}
 
-		this.step();
+		if (movedAny) {
+			this.addNewTile();
+			this.draw();
+		}
 	}
 
-	registerTileInteractive(tile, index) {
-		tile.addEventListener('click', (event) => {
-			this.cache.step = index;
-			tile.classList.toggle('clicked');
+	moveTile(index, dirx, diry) {
+		const xpos = index % this.gridsize;
+		const ypos = Math.floor(index / this.gridsize);
 
-			const palette = dustdb.palettes[Math.floor(Math.random() * dustdb.palettes.length)];
-			this.init(palette);
-		});
-	}
-
-	step() {
-		if (!this.active) {
-			return;
+		let x = xpos;
+		let y = ypos;
+		if (dirx && x + dirx >= 0 && x + dirx < this.gridsize) {
+			x += dirx;
 		}
 
-		this.tiles[this.cache.step].style.backgroundColor = '#' + this.randomColor();
+		if (diry && y + diry >= 0 && y + diry < this.gridsize) {
+			y += diry;
+		}
 
-		this.cache.step = (this.cache.step + 1) % this.tiles.length;
+		const newindex = y * this.gridsize + x;
+		if (newindex !== index) {
+			let allowmove = true;
+			if (this.tiles[newindex]) {
+				allowmove = this.tiles[newindex].pow === this.tiles[index].pow;
+				if (allowmove) {
+					this.tiles[index].pow += 1;
+					cssAnimate(this.container.children[newindex], 'animate-merge', 500);
+				}
+			}
 
-		setTimeout(() => this.step(), 707);
+			if (allowmove) {
+				this.tiles[newindex] = this.tiles[index];
+				this.tiles[index] = null;
+			}
+
+			return allowmove;
+		}
+
+		return false;
 	}
 
-	randomColor() {
-		return this.palette[Math.floor(this.palette.length * Math.random())];
+	draw() {
+		for (const i in this.tiles) {
+			const el = this.container.children[i];
+
+			const tile = this.tiles[i];
+			if (tile) {
+				el.dataset.tile = tile.title;
+			} else {
+				delete el.dataset.tile;
+			}
+		}
 	}
+
+	addNewTile() {
+		let pos = Math.floor(random.next() * this.tiles.length);
+		while (this.tiles[pos]) {
+			pos = (pos + 1) % this.tiles.length;
+		}
+
+		const tile = new Game2048Tile();
+		if (random.next() > 0.9) {
+			tile.pow += 1;
+		}
+		this.tiles[pos] = tile;
+		cssAnimate(this.container.children[pos], 'animate-appear', 500);
+	}
+}
+
+function genTiles(messages) {
+	const pixels = 512;
+	const tile = 128;
+	const tiles = Math.pow(pixels / tile, 2);
+
+	let content = '';
+	for (let i = 0; i < tiles; i++) {
+		content += '<tile></tile>';
+	}
+
+	messages.find('game').write(content);
 }
 
 function main() {
@@ -79,22 +158,24 @@ function main() {
 	messages.event('postinit');
 	messages.state('game');
 
-	const pixels = 512;
-	const tile = 64;
-	const tiles = Math.pow(pixels / tile, 2);
-
-	let content = '';
-	for (let i = 0; i < tiles; i++) {
-		content += '<tile></tile>';
-	}
-
-	messages.find('game').write(content);
-
-	dust0();
-	dustdb.palettes = parseArgList(document.querySelector('db#palette_list').innerHTML);
-	new Dust0().init('tyndustre_220422_00').exec();
-
-	//initHilo();
+	genTiles(messages);
+	const game2048 = new Game2048(messages.find('game').contentNode).init().run();
+	document.addEventListener('keydown', (ev) => {
+		switch (ev.code) {
+			case 'ArrowRight':
+				game2048.step(1, 0);
+				break;
+			case 'ArrowLeft':
+				game2048.step(-1, 0);
+				break;
+			case 'ArrowUp':
+				game2048.step(0, -1);
+				break;
+			case 'ArrowDown':
+				game2048.step(0, 1);
+				break;
+		}
+	});
 }
 
 main();
